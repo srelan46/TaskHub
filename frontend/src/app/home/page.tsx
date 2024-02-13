691f10183bb97489be96fffdcc5c0ec7f2d0504b
srelan46 <srelan46@gmail.com>
2024-02-12 22:46:16 -0500

    Added Sidebar to Homepage

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
 import TaskList from "../components/taskList";
-
+import SideBar from "../components/SideBar"
 export default function App() {
     return (
         <div>
+            <SideBar />
             <h1>
                 Welcome to Task Hub
             </h1>
